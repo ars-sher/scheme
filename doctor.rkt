8f1edfd75a35d4bee1847bf6825c9246965e8d0a
Arseny Sher <arsv.sher@gmail.com>
2014-09-14 20:54:21 +0400

    doctor code formatted

--- doctor.rkt
+++ doctor.rkt
@@ -3,55 +3,100 @@
   (define (doctor-driver-loop name)
     (define (reply user-response)
       (define (change-person phrase)
-        (many-replace '((i you) (me you) (am are) (my your)) phrase))
+        (many-replace '((i you) (me you) (am are) (my your)) phrase)
+      )
 
       (define (qualifier)
-        (pick-random '((you seem to think)
-                       (you feel that)
-                       (why do you believe)
-                       (why do you say))))
+        (pick-random
+          '(
+            (you seem to think)
+            (you feel that)
+            (why do you believe)
+            (why do you say)
+          )
+        )
+      )
 
       (define (hedge)
-        (pick-random '((please go on)
-                       (many people have the same sorts of feelings)
-                       (many of my patients have told me the same thing)
-                       (please continue))))
+        (pick-random
+          '(
+            (please go on)
+            (many people have the same sorts of feelings)
+            (many of my patients have told me the same thing)
+            (please continue)
+          )
+        )
+      )
       
-      (cond ((fifty-fifty)
-             (append (qualifier) (change-person user-response)))
-            (else (hedge))))
+      (cond 
+        ((fifty-fifty)
+             (append (qualifier) (change-person user-response))
+        )
+        (else 
+          (hedge)
+        )
+      )
+    )
 
     (newline)
     (print '**)
     (let ((user-response (read)))
-      (cond ((equal? user-response '(goodbye))
-             (printf "Goodbye, ~a!\n" name)
-             (print '(see you next week)))
-            (else (print (reply user-response))
-                  (doctor-driver-loop name)))))
+      (cond
+        ((equal? user-response '(goodbye))
+          (printf "Goodbye, ~a!\n" name)
+          (print '(see you next week))
+        )
+        (else
+          (print (reply user-response))
+          (doctor-driver-loop name)
+        )
+      )
+    )
+  )
 
   (printf "Hello, ~a!\n" name)
   (print '(what seems to be the trouble?))
-  (doctor-driver-loop name))
+  (doctor-driver-loop name)
+)
 
 (define (fifty-fifty)
-  (= (random 2) 0))
+  (= (random 2) 
+     
+     0)
+)
 
 (define (replace pattern replacement lst)
-  (cond ((null? lst) '())
-        ((equal? (car lst) pattern)
-         (cons replacement
-               (replace pattern replacement (cdr lst))))
-        (else
-         (cons (car lst)
-               (replace pattern replacement (cdr lst))))))
+  (cond
+    ((null? lst) '())
+    ((equal? (car lst) pattern)
+      (cons
+        replacement
+        (replace pattern replacement (cdr lst))
+      )
+    )
+    (else
+      (cons
+        (car lst)
+        (replace pattern replacement (cdr lst)))
+    )
+  )
+)
 
 (define (pick-random lst)
-  (list-ref lst (random (length lst))))
+  (list-ref lst (random (length lst)))
+)
 
 (define (many-replace replacement-pairs lst)
-        (cond ((null? replacement-pairs) lst)
-              (else (let ((pat-rep (car replacement-pairs)))
-                      (replace (car pat-rep)
-                               (cadr pat-rep)
-                               (many-replace (cdr replacement-pairs) lst))))))   
+  (cond 
+    ((null? replacement-pairs) lst)
+    (else
+      (let ((pat-rep (car replacement-pairs)))
+        (replace
+          (car pat-rep)
+          (cadr pat-rep)
+          (many-replace (cdr replacement-pairs) lst)
+        )
+      )
+    )
+  )
+)   
